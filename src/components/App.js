af059654bf72d124caf7531aebee932b8c6a2df0
evansinho <igiri.evanson@gmail.com>
2020-02-05 12:29:33 +0100

    updated a package

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import Contacts from "./Contacts";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Contacts />
+        <h1>Hello world</h1>
       </div>
     );
   }
